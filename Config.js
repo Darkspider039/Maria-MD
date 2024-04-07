ee13fa6ce742b9a373c52ae4a8fb7ce976f1ee9a
Darkspider039 <166133440+Darkspider039@users.noreply.github.com>
2024-04-07 06:00:52 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['94788303001']
+global.ownername = "💙𓆩‎✘Thisara𓆪_✰"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Sri lankan"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = '𝐃𝐀𝐑𝐊-𝐒𝐏𝐈𝐃𝐄𝐑-𝐌𝐃' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = '💙𓆩‎✘Thisara𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = '𝐓𝐇𝐈𝐒𝐀𝐑𝐀 Bot'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
